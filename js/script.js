27d132bf51116c3ff155a57d695b61fe3d71ce8f
wasily <wasnik7819@gmail.com>
2023-08-27 09:38:10 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,73 @@
+"use strict";
+
+
+
+// const leftBorderWidth = 1;
+
+// console.log(leftBorderWidth);
+
+// // alert(5);
+// // [].push('a');
+
+// let number = 4.6;
+
+// console.log(-4/0);
+// console.log('string' * 8);
+
+// const obj = {
+// 	name: 'John',
+// 	age: 45,
+// 	isMarried: false
+// };
+
+// console.log(obj.isMarried);
+
+// let array = ['plum.png', 'orange.png', 'apple.jpg'];
+
+// console.log(array[2]);
+
+// alert('Hello');
+
+// const result = confirm('Are you here?');
+
+// console.log(result);
+
+// const answer = +prompt('Вам есть 18?', '16');
+
+// console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как ваша фамилия?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+
+// document.write(answers);
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = 'Wasily';
+
+// alert(`Привет, ${user}`);
+
+// let incr = 10,
+// 	decr = 10;
+
+// incr++;
+// decr--;
+
+// console.log(++incr);
+// console.log(--decr);
+
+// const isChecked = true,
+// 		isClose = true;
+
+// console.log(isChecked && isClose);
+
+
+const isChecked = false,
+		isClose = false;
+
+console.log(isChecked || !isClose);
